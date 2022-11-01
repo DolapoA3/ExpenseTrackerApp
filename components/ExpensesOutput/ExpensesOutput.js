0e074d26ac1a1f755d20033c56087fc456048d47
LeoKing33 <lapo8387@gmail.com>
2022-10-31 20:06:54 -0500

    closing modal and managing app-wide state with context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,63 +4,6 @@ import { GlobalStyles } from '../../constants/styles';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-19'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 21.99,
-    date: new Date('2022-01-14'),
-  },
-  {
-    id: 'e3',
-    description: 'A scarf',
-    amount: 5.89,
-    date: new Date('2022-10-10'),
-  },
-  {
-    id: 'e4',
-    description: 'Some strawberries',
-    amount: 4.38,
-    date: new Date('2021-03-22'),
-  },
-  {
-    id: 'e5',
-    description: 'Aloe Vera plant',
-    amount: 25.89,
-    date: new Date('2022-02-18'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of trousers',
-    amount: 21.99,
-    date: new Date('2021-01-16'),
-  },
-  {
-    id: 'e7',
-    description: 'A scarf',
-    amount: 5.89,
-    date: new Date('2022-10-10'),
-  },
-  {
-    id: 'e8',
-    description: 'Some strawberries',
-    amount: 4.38,
-    date: new Date('2021-03-22'),
-  },
-  {
-    id: 'e9',
-    description: 'Aloe Vera plant',
-    amount: 25.89,
-    date: new Date('2022-02-18'),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Button from '../components/UI/Button';
 
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
@@ -14,11 +15,26 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
-      
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode='flat' onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -40,6 +56,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
